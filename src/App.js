e94ffe076c536137048da39067784e0571c1127b
chu-young <ttheko101@gmail.com>
2021-07-10 19:26:25 +0200

    added button styles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <div>Hello</div>
+    <div className='container'>
+
+
+<button>More Info</button>
+
+
     </div>
+
   );
 }
 
