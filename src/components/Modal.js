062670b7a6d162f9e9206c0dd9ed4edf49cf4ed2
chu-young <ttheko101@gmail.com>
2021-07-13 12:43:42 +0200

    modal complete

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,13 +8,19 @@ return(
 {props.showModal ? (
     <div className="background">
         <div className='modalWrapper' showModal={props.showModal}>
-            <img clasName="modalImg" src={require('./deals.jpg')} alt='A landscape'/>
+
+            <img clasName="modalImg" src={require('./deals.jpg').default} alt="A Landscape" width="100%" height="100%"/>
+          
             <div className="modalContent">
-                <h1>Are You ready?</h1>
-                <p>Everything you need right here</p>
-                <button>Join Now</button>
+                <h1>React Modal</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus ipsa, repellat doloremque debitis ipsum illo voluptates recusandae rem nulla cupiditate ducimus quidem optio rerum expedita porro non sed? Harum repellat iste atque, labore odio vitae quisquam deserunt expedita architecto impedit!</p>
+                <p>Made with: React, XML, JQuery and The New York Times API</p>
+                <div className="buttonLayout">
+                <button className="innerModalButton">View Code</button>
+                <button className="innerModalButton">Live Site</button>
+                </div>
             </div>
-            <div className="closeModalButton" aria-label="Close modal" onClick={()=> props.setShowModal(prev => !prev)}></div>
+            <button className="closeModalButton" aria-label="Close modal" onClick={()=> props.setShowModal(prev => !prev)}>Close</button>
         </div>
     </div>
 ) : null} 
